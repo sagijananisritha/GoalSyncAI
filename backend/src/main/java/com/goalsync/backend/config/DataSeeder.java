e05bb49592722ab8514c7b11cb4218e402044900
sagijananisritha <sagijananisritha@gmail.com>
2026-05-18 15:22:30 +0530

    added frontend

--- backend/src/main/java/com/goalsync/backend/config/DataSeeder.java
+++ backend/src/main/java/com/goalsync/backend/config/DataSeeder.java
@@ -11,7 +11,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
